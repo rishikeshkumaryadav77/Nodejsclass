4a804c8a76c7acb10862508e1b42f587faf63651
rishikeshkumaryadav77 <krishikes605@gmail.com>
2025-05-14 23:33:48 +0530

    common js

--- nodejsYoutube/sum.js
+++ nodejsYoutube/sum.js
@@ -1,11 +1,19 @@
-function sum(a, b){
-  return a+b;
+// function sum(a, b){
+//   return a+b;
+// }
+
+
+// function sumParent(text, a, b, sum){
+//   const result = sum(a,b)
+//   console.log(text,result)
+// }
+
+// sumParent('The sum of a+b = ', 5, 7, sum);
+
+
+//exporting module as common js
+function greet(){
+  return 'Hello, World!'
 }
 
-
-function sumParent(text, a, b, sum){
-  const result = sum(a,b)
-  console.log(text,a, b, result)
-}
-
-sumParent('hii this sum of a+b = ', 5, 7, sum);
+module.exports = greet;

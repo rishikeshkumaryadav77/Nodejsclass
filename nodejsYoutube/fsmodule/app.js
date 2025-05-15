c9ac99cadf838d5083fb52f296f2db0324790a0f
rishikeshkumaryadav77 <krishikes605@gmail.com>
2025-05-15 13:22:48 +0530

    writefile

--- nodejsYoutube/fsmodule/app.js
+++ nodejsYoutube/fsmodule/app.js
@@ -2,10 +2,17 @@
 
 const fs = require('fs')
 
-// fs.readFile('input.txt', 'utf-8', (err,data) => {
+// fs.readFile('input.txt', 'utf-8', (err,data) => {      //asynchronous 
 //   if(err) throw(err);
 //   console.log(data)
 // })
 
-const data = fs.readFileSync('input.txt', 'utf-8');
-console.log(data)
+// const data = fs.readFileSync('input.txt', 'utf-8');  //synchronous
+// console.log(data)
+
+
+//fs.writefile
+
+fs.writeFile('input.txt', 'removed content', (err) =>{
+  if(err) throw(err)
+})

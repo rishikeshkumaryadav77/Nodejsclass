e48e169476bdb16a11d509bd2db8d2dc3042c60d
rishikeshkumaryadav77 <krishikes605@gmail.com>
2025-05-15 13:11:44 +0530

    readflesync

--- nodejsYoutube/fsmodule/app.js
+++ nodejsYoutube/fsmodule/app.js
@@ -2,7 +2,10 @@
 
 const fs = require('fs')
 
-fs.readFile('input.txt', 'utf-8', (err,data) => {
-  if(err) throw(err);
-  console.log(data)
-})
+// fs.readFile('input.txt', 'utf-8', (err,data) => {
+//   if(err) throw(err);
+//   console.log(data)
+// })
+
+const data = fs.readFileSync('input.txt', 'utf-8');
+console.log(data)

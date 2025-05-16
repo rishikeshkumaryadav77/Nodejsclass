42c784d6323aa8ab7f17b1ccfad88c137aff527e
rishikeshkumaryadav77 <krishikes605@gmail.com>
2025-05-16 10:44:20 +0530

    req.url

--- nodejsYoutube/http/app.js
+++ nodejsYoutube/http/app.js
@@ -1,8 +1,26 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   //  console.log(`Request received: ${req.method} ${req.url}`);
-   res.end('hello');
+   if(req.url === '/'){
+    res.end('hello');
+   }
+   else if(req.url === '/user'){
+    const user = {
+      name:'shiva',
+      age:15,
+      address:'hyderabad'
+    }
+    console.log(user)
+    res.writeHead(200, {'Content-Type':'application/json'}) //provide pretty
+    res.end(JSON.stringify(user))
+    
+   }
+   
 });
+
+
+
+
 
 server.listen(8000, () => {
    console.log('Server started on port 8000');

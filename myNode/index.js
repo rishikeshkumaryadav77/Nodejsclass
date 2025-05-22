f0c362770f627cfaaaf837bdc54e9630e7f716f6
rishikeshkumaryadav77 <krishikes605@gmail.com>
2025-05-22 14:48:05 +0530

    its only show data

--- myNode/index.js
+++ myNode/index.js
@@ -1,5 +1,14 @@
 const express = require("express")
+const multer = require('multer')
 const app = express()
+
+// urlencoded sended data
+app.use(express.urlencoded({extended:true}))
+
+//for data sended
+const upload = multer()
+// app.use(upload.array()) //onlu used for text
+app.use(upload.single('image'))  //used for file as well as
 
 //diret access file bu hitting the url on the server
 app.use('/upload',express.static('upload'))
@@ -8,6 +17,13 @@ app.get("/", (req,res) =>{
   res.send('home page')
 })
 
+
+//form data
+app.post("/form", (req, res)=>{
+  const data = req.body
+  console.log(data)
+  res.send("dta submitted successfully")
+})
 
 app.listen(8000, ()=>{
   console.log("server runs on => http://localhost:8000")
